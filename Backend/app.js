a419972c5f57e0c6caa0cab92d00671a074f9751
RichardNzembei <richardsonreuben78@gmail.com>
2024-12-07 14:05:07 +0300

    update

--- Backend/app.js
+++ Backend/app.js
@@ -2,21 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const stockRoutes = require('./routes/stocks');
-const salesRoutes= require('./routes/sales')
-
-
+const salesRoutes = require('./routes/sales');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = ['https://budget-hair-stock-management-system.vercel.app'];
 
 app.use(bodyParser.json());
-app.use(cors());
 
-// Use routes
+app.use(cors({
+  origin: function(origin, callback) {
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS policy error: Origin not allowed'));
+    }
+  }
+}));
+
 app.use('/api', stockRoutes);
-app.use('/api', salesRoutes)
-
-
+app.use('/api', salesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
