1275df172838f823fceb6c5cdad15ffac3b40464
RichardNzembei <richardsonreuben78@gmail.com>
2024-12-28 20:37:02 +0300

    update

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
     origin: [
       "https://budget-hair-stock-management-system-wui7.vercel.app",
       "https://budget-hair-stock-management-system.vercel.app",
-      "https://budget-hair-stock-management-system-ll2i.vercel.app/",
+      "https://budget-hair-stock-management-system-ll2i.vercel.app",
       "http://localhost:5173",
       "http://localhost:5174",
     ],
@@ -31,7 +31,7 @@ app.use(
     origin: [
       "https://budget-hair-stock-management-system-wui7.vercel.app",
       "https://budget-hair-stock-management-system.vercel.app",
-      "https://budget-hair-stock-management-system-ll2i.vercel.app/",
+      "https://budget-hair-stock-management-system-ll2i.vercel.app",
       "http://localhost:5173",
       "http://localhost:5174",
     ],
