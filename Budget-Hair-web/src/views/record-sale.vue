d038ee1d41d9da5af199cfc77b030ac569c714e1
R_Nzembei <richardsonreuben78@gmail.com>
2025-09-09 14:53:13 +0300

    Update record-sale.vue

--- Budget-Hair-web/src/views/record-sale.vue
+++ Budget-Hair-web/src/views/record-sale.vue
@@ -98,17 +98,6 @@
                     {{ record.time }}
                   </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <button 
-                    @click="deleteSale(record.id, record.productType, record.productSubtype, record.quantitySold)"
-                    class="text-blue-600 hover:text-blue-900 mr-4 inline-flex items-center"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                    </svg>
-                    Restore
-                  </button>
-                </td>
               </tr>
             </tbody>
           </table>
