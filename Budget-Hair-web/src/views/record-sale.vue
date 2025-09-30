76a0197b7eca69237e1367d4fd7180827b6c19d3
RichardNzembei <richardsonreuben78@gmail.com>
2025-09-30 18:30:30 +0300

    sales restore and time UI

--- Budget-Hair-web/src/views/record-sale.vue
+++ Budget-Hair-web/src/views/record-sale.vue
@@ -90,6 +90,14 @@
                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
                   {{ record.date }}
                 </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    {{ record.time }}
+                  </div>
+                </td>
                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                   <button
                       @click="deleteSale(record.id, record.productType, record.productSubtype, record.quantitySold)"
@@ -100,14 +108,6 @@
                     </svg>
                     Restore
                   </button>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
-                  <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    {{ record.time }}
-                  </div>
                 </td>
               </tr>
             </tbody>
